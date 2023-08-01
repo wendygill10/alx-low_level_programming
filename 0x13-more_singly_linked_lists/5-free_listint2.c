c06f5a30a1c585e0e8798e2d7a7749d0ea9e7e68
Wendy <gillwendy10@gmail.com>
2023-08-01 12:07:41 -0700

    Singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to listint_t list is freed
+ * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
@@ -14,8 +14,8 @@ void free_listint2(listint_t **head)
 	while (*head)
 	{
 		temp = (*head)->next;
-		free(*head)
-			* head = temp;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
